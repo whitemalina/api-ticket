980c1c87bdc5080e8cd8806f7bf01ba4a3e6cdc7
WhiteMalina <49318023+whitemalina@users.noreply.github.com>
2022-10-14 12:40:59 +0500

    Update NewEventController.php

--- app/Http/Controllers/Api/NewEventController.php
+++ app/Http/Controllers/Api/NewEventController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
